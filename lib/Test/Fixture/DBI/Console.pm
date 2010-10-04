3f0d3cfbe5791554029abcb06a1b597b7f740f9d
Yoshihiro Sasaki <aloelight@gmail.com>
2010-10-04 22:43:48 +0900

    initial commit

---
+++ lib/Test/Fixture/DBI/Console.pm
@@ -0,0 +1,32 @@
+package Test::Fixture::DBI::Console;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Test::Fixture::DBI::Console -
+
+=head1 SYNOPSIS
+
+  use Test::Fixture::DBI::Console;
+
+=head1 DESCRIPTION
+
+Test::Fixture::DBI::Console is
+
+=head1 AUTHOR
+
+aloelight E<lt>aloelight {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
